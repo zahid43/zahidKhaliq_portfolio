c12da1cb613eca0980e61b558a7241693efc2bbb
Zahid K <Zaahid.khaliq@gmail.com>
2026-05-04 00:15:49 +0500

    remove unused CSS classes and add social links configuration constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,19 @@
 export const navLinks = ["Home", "About me", "Experience", "Projects", "Endorsements", "Contact"];
+
+export const socialLinks = [
+  {
+    href: "https://linkedin.com/in/zaahidkhaliq",
+    label: "LinkedIn",
+    icon: "LinkedinIcon",
+  },
+  {
+    href: "https://github.com/zaahidkhaliq",
+    label: "GitHub",
+    icon: "GithubIcon",
+  },
+  {
+    href: "https://x.com/zaahidkhaliq",
+    label: "X / Twitter",
+    icon: "XIcon",
+  },
+];
